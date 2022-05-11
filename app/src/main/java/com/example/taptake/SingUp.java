da3e419d09b5a62cdbf59377558b71f3cc4a1c11
Alex Cohen <alex-dambros@hotmail.com>
2022-05-11 16:51:55 -0300

    Added Login, added news icons, added sign up function

---
+++ app/src/main/java/com/example/taptake/SingUp.java
@@ -0,0 +1,14 @@
+package com.example.taptake;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SingUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+    }
+}
--- app/src/main/java/com/example/taptake/SplashScreen.java
+++ app/src/main/java/com/example/taptake/SplashScreen.java
@@ -2,6 +2,8 @@ package com.example.taptake;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +16,26 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonLoading = findViewById(R.id.buttonLoading);
-        buttonLoading.setOnClickListener(view -> openHomeScreen());
+        buttonLoading.setOnClickListener(view -> openLoginScreen());
+
+        Button singUp = findViewById(R.id.singUp);
+        singUp.setOnClickListener(view -> openSingUpScreen());
+
+        getWindow().setDecorFitsSystemWindows(false);
+        final WindowInsetsController insetsController = getWindow().getInsetsController();
+        if (insetsController != null) {
+            insetsController.hide(WindowInsets.Type.statusBars());
+            insetsController.setSystemBarsBehavior(insetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
+
     }
 
-    public void openHomeScreen(){
-        Intent intent = new Intent(this, HomeScreen.class);
+    public void openLoginScreen(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+    public void openSingUpScreen(){
+        Intent intent = new Intent(this, SingUp.class);
         startActivity(intent);
     }
 
