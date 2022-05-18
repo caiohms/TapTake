78b5657a626c0a9475dbd2d9798fd0ebf0a7398a
Alex Cohen <alex-dambros@hotmail.com>
2022-05-18 19:52:50 -0300

    edit profile, select photo in gallery

--- app/src/main/java/com/example/taptake/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/taptake/ui/profile/ProfileFragment.java
@@ -1,10 +1,10 @@
 package com.example.taptake.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,6 +15,7 @@ import com.example.taptake.databinding.FragmentProfileBinding;
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    int SELECT_PICTURE = 200;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,9 +26,24 @@ public class ProfileFragment extends Fragment {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textProfile;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.buttonSaveProfile.setOnClickListener(view -> clearEditText());
+        binding.buttonSelectImage.setOnClickListener(view -> imageChooser());
+
         return root;
+    }
+    public void clearEditText(){
+        binding.profileEditTextName.getText().clear();
+        binding.profileEditTextEmail.getText().clear();
+        binding.profileEditTextPassword.getText().clear();
+        binding.profileEditTextPasswordConfirm.getText().clear();
+
+    }
+
+    public void imageChooser() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
     }
 
     @Override
